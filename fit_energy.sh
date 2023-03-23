5ea54807c4b6e08eae71deb4ed51c013e080f71f
Leonardo Razzai - leonardo.razzai@studio.unibo.it <STUDENTI#leonardo.razzai@Str957-cluster.personale.dir.unibo.it>
2023-03-23 09:24:23 +0100

    Select correct column for alat(a.u.) in murn*.out

--- fit_energy.sh
+++ fit_energy.sh
@@ -18,10 +18,14 @@ paste -d" " alat_used.dat temp1 >> murn_$E.in
 rm temp*
 
 # fit data in murn.in and put result to murn.out
-murn_FHI.x < murn_$E.in > murn_$E.out
+./murn_FHI.x < murn_$E.in > murn_$E.out
 
-# write murn.dat with fitted energy(Ry) and alat(a.u.)
+# write data with fitted energy(Ry) and alat(a.u.)
 tail -500 murn_$E.out > temp
-echo 'alat(a.u.) energy(Ry)' > murn_$E.dat
-awk '{print $1, $2}' temp >> murn_$E.dat
+echo 'alat(a.u.) energy(Ry)' > en_vs_alat_fit_$E.dat
+awk '{print $3, $2}' temp >> en_vs_alat_fit_$E.dat
 rm temp
+# write experimental data
+echo 'alat(a.u.) energy(Ry)' > en_vs_alat_exp_$E.dat
+tail -7 murn_$E.in >> en_vs_alat_exp_$E.dat
+
