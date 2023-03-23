8160e19c695e62f899ff834ecf8e4da413c995fa
xReLeo <xcoderx00@gmail.com>
2023-03-23 11:18:16 +0100

    Added analysis for all ecut avalues

--- alat_analysis.py
+++ alat_analysis.py
@@ -5,7 +5,14 @@ ecut_30 = ecut_sample(30)
 ecut_30.plot_fit(color='blue')
 ecut_35 = ecut_sample(35)
 ecut_35.plot_fit(color='red')
+ecut_40 = ecut_sample(40)
+ecut_40.plot_fit(color='green')
 
+alat_min_30 = ecut_30.alat_min_en
+alat_min_35 = ecut_35.alat_min_en
+alat_min_40 = ecut_40.alat_min_en
+
+print(f"alat_min ecut=30 : {alat_min_30}\nalat_min ecut=35 : {alat_min_35}\nalat_min ecut=40 : {alat_min_40}")
 plt.xlabel('alat (a.u.)')
 plt.ylabel('energy (Ry)')
 plt.title('Energy vs lattice parameter')
