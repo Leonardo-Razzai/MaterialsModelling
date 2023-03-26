b0b8e66576f5cfde07e41da9f8f8a1e3560a1e3f
xReLeo <xcoderx00@gmail.com>
2023-03-26 19:01:09 +0200

    Python file for k_points analysis

---
+++ k_points_analysis.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('en_vs_kpoints.dat', sep=' ')
+energy = np.array(df['energy(Ry)'])
+k_points = np.array(df['k_points'])
+e_14 = energy[k_points==14]
+Delta_E = energy - e_14
+
+
+base_font = {'family': 'serif',
+        'color':  'black',
+        'size': 12,
+        }
+
+title_font = {'family': 'serif',
+        'color':  'black',
+        'size': 19,
+        'weight' : 'normal'
+        }
+
+_, ax = plt.subplots(1, 1, figsize=(10, 7))
+ax.plot(k_points, Delta_E, 'o')
+ax.set_title(r'$\Delta E$ (Ry) vs number of k-points', fontdict=title_font)
+ax.set_xlabel('k-points', fontdict=base_font)
+ax.set_ylabel(r'$\Delta E$ (Ry)', fontdict=base_font)
+ax.grid()
+plt.show()
