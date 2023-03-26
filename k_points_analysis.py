dc06d6e3a0f7b49a99456ca8fe60da5fd2126b6c
xReLeo <xcoderx00@gmail.com>
2023-03-26 19:38:44 +0200

    Now e_mean is set to 16

--- k_points_analysis.py
+++ k_points_analysis.py
@@ -5,8 +5,8 @@ import numpy as np
 df = pd.read_csv('en_vs_kpoints.dat', sep=' ')
 energy = np.array(df['energy(Ry)'])
 k_points = np.array(df['k_points'])
-e_14 = energy[k_points==14]
-Delta_E = energy - e_14
+e_mean = energy[k_points==16]
+Delta_E = energy - e_mean
 
 
 base_font = {'family': 'serif',
@@ -21,7 +21,7 @@ title_font = {'family': 'serif',
         }
 
 _, ax = plt.subplots(1, 1, figsize=(10, 7))
-ax.plot(k_points, Delta_E, 'o')
+ax.plot(k_points, Delta_E, 'o-')
 ax.set_title(r'$\Delta E$ (Ry) vs number of k-points', fontdict=title_font)
 ax.set_xlabel('k-points', fontdict=base_font)
 ax.set_ylabel(r'$\Delta E$ (Ry)', fontdict=base_font)
