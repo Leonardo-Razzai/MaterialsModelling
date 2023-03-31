6b0b3cbaf9310578e0bfc1de4c2324864813a577
xReLeo <xcoderx00@gmail.com>
2023-03-31 14:16:29 +0200

    Various

--- k_points/k_points_analysis.py
+++ k_points/k_points_analysis.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
 
-df = pd.read_csv('en_vs_kpoints.dat', sep=' ')
+df = pd.read_csv('en_vs_kpoints_1.dat', sep=' ')
 energy = np.array(df['energy(Ry)'])
 k_points = np.array(df['k_points'])
-e_mean = energy[k_points==16]
-Delta_E = energy - e_mean
+Delta_E = (energy - energy[-1]) * 13.6 *1000 # in mev
 
 f_inter = interp1d(k_points, Delta_E, kind='cubic')
 
@@ -23,11 +22,11 @@ title_font = {'family': 'serif',
         }
 
 k = np.linspace(np.min(k_points), np.max(k_points), 300)
-_, ax = plt.subplots(1, 1, figsize=(10, 7))
+_, ax = plt.subplots(1, 1, figsize=(9, 6))
 ax.plot(k_points, Delta_E, 'o', color='royalblue')
 ax.plot(k, f_inter(k), '-', color='royalblue')
-ax.set_title(r'$\Delta E$ (Ry) vs number of k-points', fontdict=title_font)
+ax.set_title(r'$\Delta E$ (mev) vs number of k-points', fontdict=title_font)
 ax.set_xlabel('k-points', fontdict=base_font)
-ax.set_ylabel(r'$\Delta E$ (Ry)', fontdict=base_font)
+ax.set_ylabel(r'$\Delta E$ (mev)', fontdict=base_font)
 ax.grid()
 plt.show()
