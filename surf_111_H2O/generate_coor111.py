e8d08013554fceb76d12ea4513699ee037e3ed6a
Leonardo Razzai <leonardo.razzai@studio.unibo.it>
2023-04-22 18:07:18 +0200

    New folder for H2O on 111 surface simulation

---
+++ surf_111_H2O/generate_coor111.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sys
+
+def print_point(file, point):
+  file.write(f"Rh   {point[0]:.7f}   {point[1]:.7f}   {point[2]:.7f}\n")
+  
+def get_celldm3(points):
+  empty_space = 12 # Angstrom
+  last_point = points[len(points)-1]
+  zmax = last_point[2]
+  celldm3 = (zmax + empty_space) / a
+  return celldm3
+ 
+n_layers = int(sys.argv[1])
+
+a_bohr = 7.2447 / np.sqrt(2)
+a = a_bohr * 0.529177249 # Lattice parameter Angstrom
+a3 = a * np.array([0, 0, np.sqrt(6)])
+
+base_points = [
+  a * np.array([0, 0, 0]), # A
+  a * np.array([0, np.sqrt(3) / 3, 0]), # B
+  a * np.array([1/2, np.sqrt(3) / 6, 0]), # C
+]
+
+points = []
+with open(f'coor_111_{n_layers}.dat', 'w') as file:
+  for i in range(n_layers):
+    point_index = i % 3
+    selected_point = base_points[point_index]
+    inc_point = selected_point + i * a3 / 3
+    points.append(inc_point)
+    print_point(file=file, point=inc_point)
+
---
+++ surf_111_H2O/get_relax_coor111.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def print_point(file, point):
+  file.write(f"Rh   {point[0]:.7f}   {point[1]:.7f}   {point[2]:.7f}\n")
+
+def get_relaxed_coor(relaxed_coor_file_name)->list:
+  begin_index = 0
+  end_index = 0
+  relaxed_coor_list = []
+  with open(f'./{relaxed_coor_file_name}.pwo', 'r') as fp:
+    lines = fp.readlines()
+    for i, line in enumerate(lines):
+      if line.strip() == 'Begin final coordinates':
+        begin_index = i
+      if line.strip() == 'End final coordinates':
+        end_index = i
+    
+    for i in range(begin_index + 3, end_index):
+      line = lines[i]
+      coor_string = line.split()[1:]
+      relaxed_coor = np.array([
+        float(coor_string[0]),
+        float(coor_string[1]),
+        float(coor_string[2])
+      ])
+      
+      relaxed_coor_list.append(relaxed_coor)
+    
+  return relaxed_coor_list
+
+def write_new_coor(file_name='new_coor.dat'):
+  
+  a_bohr = 7.2447 / np.sqrt(2)
+  a = a_bohr * 0.529177249 # Lattice parameter Angstrom
+  a1 = a * np.array([1, 0, 0])
+  a2 = a * np.array([-1/2, np.sqrt(3)/2, 0])
+
+  new_coord_list = []
+  for relaxed_coor in relaxed_coor_list:
+    new_coord_list.append(relaxed_coor)
+    new_coord_list.append(relaxed_coor + a1)
+    new_coord_list.append(relaxed_coor + a2)
+    new_coord_list.append(relaxed_coor + a1 + a2)
+
+  with open(file_name, 'w') as file:
+    for coor in new_coord_list:
+      print_point(file, coor)
+
+relaxed_coor_file_name = 'n_layers4' # file.pwo with relaxed coordinates
+relaxed_coor_list = get_relaxed_coor(relaxed_coor_file_name)
+write_new_coor('surf_coor.dat')
