9eb18bdf33991d554341f618f41c844e7093ba17
Leonardo Razzai <leonardo.razzai@studio.unibo.it>
2023-05-08 11:59:00 +0200

    Data for analysis change distance

--- surf_111_H2O/analysis_dist/get_data.sh
+++ surf_111_H2O/analysis_dist/get_data.sh
@@ -9,9 +9,8 @@ echo Final height:
 read dist_f
 
 echo "dist E_tot" > data/data_dist.dat
-E_tot=1
 for dist in $(seq $dist_f 0.5 $dist_i)
 do
-  # E_tot=$(grep -a Final ../diss_at_$dist.pwo | awk '{print $4}')
+  E_tot=$(grep -a Final ../diss_at_$dist.pwo | awk '{print $4}')
   echo "$dist $E_tot" >> data/data_dist.dat
 done
