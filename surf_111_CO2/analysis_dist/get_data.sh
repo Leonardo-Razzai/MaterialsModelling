d9f9a11691659be1a5f87d11137d328fb40b3c46
Leonardo Razzai <leonardo.razzai@studio.unibo.it>
2023-07-09 10:56:20 +0200

    Add scripts related to change dist from surf CO2

---
+++ surf_111_CO2/analysis_dist/get_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create  a file called data_dist.dat with total final energy (E_tot)
+# for each distance (dist) of Oxigen from surface
+
+echo Initial height: 
+read dist_i
+echo Final height:
+read dist_f
+
+echo "dist E_tot" > data/data_dist.dat
+for dist in $(seq $dist_f 0.5 $dist_i)
+do
+  E_tot=$(grep -a Final ../diss_at_$dist.pwo | awk '{print $4}')
+  echo "$dist $E_tot" >> data/data_dist.dat
+done
---
+++ surf_111_CO2/get_dist_coordinates.py
@@ -0,0 +1,112 @@
+import numpy as np
+import sys
+
+num_layers = 4
+num_cells = 2
+num_atoms_per_layer = num_cells * num_cells
+# NB: give integer numbers as .0
+height_from_surf = float(sys.argv[1])
+
+a_bohr = 7.2447 / np.sqrt(2)
+a = a_bohr * 0.529177249
+a1 = a * np.array([1, 0, 0])
+a2 = a * np.array([-1/2, np.sqrt(3)/2, 0])
+
+def print_atom_coor(file, 
+                    atom: str, 
+                    coor: np.ndarray, 
+                    write=True):
+  
+  if write == True:
+    file.write(f"{atom}   {coor[0]:.7f}   {coor[1]:.7f}   {coor[2]:.7f}\n")
+  else:
+    return f"{atom}   {coor[0]:.7f}   {coor[1]:.7f}   {coor[2]:.7f}"
+  
+def get_relaxed_coor(relaxed_coor_file_name)->list:
+  '''
+  Given final coordinates in relaxed_coor_file_name.pwo
+  a list of ndarrays, each containing a coordinate, is returned.
+  '''
+  begin_index = 0
+  end_index = 0
+  relaxed_coor_list = []
+  with open(f'./{relaxed_coor_file_name}.pwo', 'r') as fp:
+    lines = fp.readlines()
+    for i, line in enumerate(lines):
+      if line.strip() == 'Begin final coordinates':
+        begin_index = i
+      if line.strip() == 'End final coordinates':
+        end_index = i
+    
+    for i in range(begin_index + 3, end_index):
+      line = lines[i]
+      coor_string = line.split()[1:]
+      relaxed_coor = np.array([
+        float(coor_string[0]),
+        float(coor_string[1]),
+        float(coor_string[2])
+      ])
+      
+      relaxed_coor_list.append(relaxed_coor)
+    
+  return relaxed_coor_list
+
+def write_surf_coor(file, 
+                    surf_coor_list: list):
+  
+  for i in range(len(surf_coor_list)):
+    coor = surf_coor_list[i]
+    if i < (num_cells**2 - 1) * num_layers:
+      print_atom_coor(file, 'Rh', coor)
+    elif i >= (num_cells**2 - 1) * num_layers:
+      line = print_atom_coor(file, 'Rh', coor, write=False)
+      file.write(line + '  1  1  0\n')
+
+def get_last_layers(surf_coor_list: list,
+                    num=3) -> list:
+  '''
+  Returns a list containing the last num layers of the surface
+  '''
+  layers =  []
+  for i in range(num):
+    layers.append(surf_coor_list[i * num_atoms_per_layer: (i + 1) * num_atoms_per_layer])
+  
+  return layers
+  
+def write_CO2_coor(file, 
+                   surf_coor_list: list):
+  
+  bond_length = 1.1770702732 # A
+  
+  last_layers = get_last_layers(surf_coor_list, num=3)
+  OT = last_layers[0][3]
+
+  CO2_height = OT[2] + height_from_surf
+  
+  C_base = np.array([0, 0, 0])
+  O1_base = np.array([1, 0, 0]) * bond_length
+  O2_base = np.array([-1, 0, 0]) * bond_length
+  
+  offset = np.array([OT[0], OT[1], CO2_height]) + a1 + a2
+  
+  C_coor = C_base + offset
+  O1_coor = O1_base + offset
+  O2_coor = O2_base + offset
+  
+  print_atom_coor(file, atom='O', coor=O1_coor)
+  print_atom_coor(file, atom='O', coor=O2_coor)
+  line = print_atom_coor(file, atom='C', coor=C_coor, write=False)
+  file.write(line + '  1  1  0\n')
+
+
+try:
+  previous_dist = height_from_surf - 0.5
+  relaxed_coor_file_name = f'dist_at_{previous_dist}'
+  relaxed_coor_list = get_relaxed_coor(relaxed_coor_file_name)
+except FileNotFoundError:
+  relaxed_coor_file_name = 'slab_2x2' # file.pwo with relaxed coordinates
+  relaxed_coor_list = get_relaxed_coor(relaxed_coor_file_name)
+
+with open(f'dist_at_{height_from_surf}-coor.dat', 'w') as file:
+  write_CO2_coor(file, relaxed_coor_list)
+  write_surf_coor(file, relaxed_coor_list)
